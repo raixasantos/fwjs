b55255bdcd487c41933653283f63e2c44a8aacca
Raíssa Santos <raissa.santos.095@ufrn.edu.br>
2022-04-12 22:46:09 -0300

    Adicionando tab em React

--- quiz/src/App.tsx
+++ quiz/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
-import { Quiz } from './components/Quiz';
+// import { Quiz } from './components/Quiz';
+import { Tab } from './components/Tab/Tab';
 
-function App() {
-  return <Quiz />
+function App() {  
+    // return <Quiz/>;
+    return <Tab/>;
 }
 
 export default App;
