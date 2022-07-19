e0a3ba44667f8e6c22f785ecb7d40da1aaaaa5aa
Raíssa Santos <raissa.santos.095@ufrn.edu.br>
2022-07-19 20:56:10 -0300

    adicionando validações

--- quiz/src/components/Tab/TabForm.vue
+++ quiz/src/components/Tab/TabForm.vue
@@ -1,19 +1,21 @@
 <template>
     <div class="container-form">
         <h2>Tabs</h2>
-        <form>
+        <form class="tab-form" @submit.prevent="submit">
             <div class="section-input">
                 <h5>Núm. de tabs</h5>
-                <cleave v-model="Number" :options="{numeral: true, numeralPositiveOnly: true}" />
+                <input type="number" min="1"
+                    @change="changeNumTabs((($event.target as HTMLInputElement).value))" />
             </div>
-            <div class="section-input">
-                <h5>Título</h5>
-                <input type="text" />          
-            </div>
-            <div class="section-input">
-                <h5>Conteúdo</h5>
-                <textarea />             
-            </div>
+            <div className="error">{{ errors.numTabs }}</div>
+                <div class="section-input">
+                    <h5>Título</h5>
+                    <input type="text" />          
+                </div>
+                <div class="section-input">
+                    <h5>Conteúdo</h5>
+                    <textarea />             
+                </div>
             <div class="section-buttons">
                 <div class="buttons">
                     <input type="submit" value="Salvar" />
@@ -24,7 +26,66 @@
 </template>
 
 <script setup lang="ts">
-    import Cleave from 'vue-cleave-component';
+import { ref, reactive } from 'vue'
+import { required, inRange, minLen } from '../../utils/validations'
+
+const options = reactive<string[]>([''])
+const texts = reactive<string[]>([''])
+const errors = reactive<{[name: string]: string}>({})
+const touched = reactive<{[name: string]: boolean}>({})
+
+function touch(name: string, value: boolean = true) {
+    touched[name] = value
+}
+
+function setError(name: string, error: string) {
+    errors[name] = error
+}
+
+function changeOption(index: number, value: string) {
+    touch(`option_${index}`)
+}
+
+function changeText(index: number, value: string) {
+    touch(`text_${index}`)
+}
+
+function changeNumTabs(value: string) {
+    const range = inRange(1, 7)
+    const validateFunc = (value: string) => required(value) || range(value)
+    if (check(value, validateFunc, 'numTabs')) {
+      const num = parseInt(value, 10)
+      
+      if (num <= options.length) {
+        for (let i = num; i < options.length; i++) {
+          touch(`option_${i}`, false) 
+          setError(`option_${i}`, '')
+          touch(`text_${i}`, false) 
+          setError(`text_${i}`, '')
+        }
+        
+        options.splice(num, options.length - num)
+        texts.splice(num, texts.length - num)
+      }
+      else {
+        const tailOptions = Array(num - options.length).fill('')
+        tailOptions.forEach(o => options.push(o))
+        const tailTexts = Array(num - options.length).fill('')
+        tailTexts.forEach(t => texts.push(t))
+      }
+    }
+}
+
+function check(value: string, validateFunc: Function, name: string) {
+    const error = validateFunc(value)
+    setError(name, error)
+    return error === null
+}
+
+
+function submit() {
+}
+
 </script>
 
 <style scoped>
---
+++ quiz/src/utils/validations.ts
@@ -0,0 +1,46 @@
+/**
+ * Verifica se uma string possui algum conteúdo (se o tamanho é > 0)
+ * @param value String a ser testada.
+ * @returns Uma mensagem de erro ou `null` se não houver erro algum.
+ */
+ export function required(value: string) {
+    if (value === undefined || value.trim().length === 0) {
+      return 'Este campo é obrigatório'
+    }
+    return null
+}
+
+/**
+ * Cria uma função de validação que verifica se uma string possui um número mínimo de caracteres.
+ * @param min Valor mínimo de caracteres da string.
+ * @returns Retorna a função para validar se uma string possui um tamanho mínimo.
+ */
+ export function minLen(min: number) {
+    // Esta é uma função de retorna uma função.
+    // A função retornada que deve ser utilizada como validação. Ela usa o parâmetro da primeira (`min`) para
+    // validar as strings que forem passadas na segunda.
+    return (value: string) => {
+      const v = value.trim()
+      if (v.length < min) {
+        return `Este campo requer pelo menos ${min} caracteres`
+      }
+      return null
+    }
+}
+
+/**
+ * Cria uma função de validação que verifica se uma string é um número que se encontra num faixa de valores.
+ * @param min Valor mínimo
+ * @param max Valor máximo
+ * @returns Retorna a função para validar se um número se encontra em uma faixa de valores.
+ */
+ export function inRange(min: number, max: number) {
+    return (value: string) => {
+      const num = parseInt(value, 10)
+      const [vmin, vmax] = min > max ? [max, min] : [min, max]
+      if (value && (isNaN(num) || num < vmin || num > vmax)) {
+        return `Este campo requer um número inteiro entre ${vmin} e ${vmax}`
+      }
+      return null
+    }
+}
